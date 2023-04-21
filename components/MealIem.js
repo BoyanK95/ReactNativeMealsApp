4b4fb5b6dd276864d68da0ecbf610bd9b9840ddf
BoyanK95 <boyan_k_95@abv.bg>
2023-04-21 18:08:43 +0300

    Added props and styles to MealItem component.

--- components/MealIem.js
+++ components/MealIem.js
@@ -1,11 +1,40 @@
-import { Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 
-function MealItem({title}) {
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
     return (
-        <View>
-            <Text>{title}</Text>
+        <View style={styles.mealItem}>
+            <Pressable>
+                <View>
+                    <Image source={{ uri: imageUrl }} style={styles.image} />
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+                <View>
+                    <Text>{duration}min.</Text>
+                    <Text>{complexity.toUpperCase()}</Text>
+                    <Text>{affordability.toUpperCase()}</Text>
+                </View>
+            </Pressable>
         </View>
     );
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+    mealItem :{
+        margin: 16,
+        borderRadius: 7,
+        overflow: 'hidden',
+        backgroundColor: 'white'
+    },
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 8
+    }
+});
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -11,7 +11,15 @@ function MealsOverviewScreen({ route }) {
     });
 
     function renderMealItem(itemData) {
-        return <MealItem title={itemData.item.title}/>
+        return (
+            <MealItem
+                title={itemData.item.title}
+                imageUrl={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                affordability={itemData.item.affordability}
+                complexity={itemData.item.complexity}
+            />
+        );
     }
 
     return (
