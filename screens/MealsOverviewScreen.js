28d10e3f150ee162dca394cd1f0f3c9f9d8ee15a
BoyanK95 <boyan_k_95@abv.bg>
2023-04-21 11:05:37 +0300

    Passed and Extracted categoryId through route prop

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -2,20 +2,21 @@ import { Text, View } from 'react-native';
 import { MEALS } from '../data/DUMMY_DATA';
 import { StyleSheet } from 'react-native';
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+    const catId = route.params.categoryId;
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen</Text>
+            <Text>Meals Overview Screen - {catId}</Text>
         </View>
     );
 }
 
 export default MealsOverviewScreen;
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16
     }
-})
+});
