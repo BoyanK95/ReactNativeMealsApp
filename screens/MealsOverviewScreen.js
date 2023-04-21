65e0c0506213441d3d3e61a6c8edb0cd4e23f842
BoyanK95 <boyan_k_95@abv.bg>
2023-04-21 17:20:07 +0300

    Created MealItem, used it to display meals (titles).

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,22 @@
-import { Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { MEALS } from '../data/DUMMY_DATA';
 import { StyleSheet } from 'react-native';
+import MealItem from '../components/MealIem';
 
 function MealsOverviewScreen({ route }) {
     const catId = route.params.categoryId;
 
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title}/>
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen - {catId}</Text>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
         </View>
     );
 }
