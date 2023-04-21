429552a50b43863bcf6df1d7cd029f0c126c427d
BoyanK95 <boyan_k_95@abv.bg>
2023-04-21 23:38:52 +0300

    Created custom navigation option styles

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <>
-            <StatusBar style='dark' />
+            <StatusBar style='light' />
             <NavigationContainer>
-              <Stack.Navigator>
-                <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
-                <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
-              </Stack.Navigator>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: '#3a2215' },
+                        headerTintColor: 'white',
+                        contentStyle: { backgroundColor: '#5c3b20r25' }
+                    }}
+                >
+                    <Stack.Screen
+                        name='MealsCategories'
+                        component={CategoriesScreen}
+                        options={{
+                            title: 'All Categories',
+                            headerStyle: { backgroundColor: '#3a2215' },
+                            headerTintColor: 'white',
+                            contentStyle: { backgroundColor: '#5c3b20r25' }
+                        }}
+                    />
+                    <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
+                </Stack.Navigator>
             </NavigationContainer>
         </>
     );
