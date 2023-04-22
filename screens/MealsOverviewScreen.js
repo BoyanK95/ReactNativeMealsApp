79e418e971a830ed91cf3ff91fde2d683240a8df
BoyanK95 <boyan_k_95@abv.bg>
2023-04-22 22:24:47 +0300

    Created initial MealDetailsScreen and created navigation to it.

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -21,6 +21,7 @@ function MealsOverviewScreen({ route, navigation }) {
 
     function renderMealItem(itemData) {
         const mealItemProps = {
+            id: itemData.item.id,
             title: itemData.item.title,
             imageUrl: itemData.item.imageUrl,
             duration: itemData.item.duration,
